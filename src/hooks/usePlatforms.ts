1268c44b864583bffb2da692e7204ed886059895
ZebroneBanda <zebrone.k@gmail.com>
2024-05-11 16:41:42 +0200

    Refactoring fetch games with react query

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
+
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+const usePlatforms = () => useQuery({
+  queryKey: ['platforms'],
+  queryFn: () => 
+    apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then(res => res.data),
+})
+
+export default usePlatforms;
---
+++ src/services/api-client.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "42143d3540dd452aa11e4b454265d18c",
+  },
+});
