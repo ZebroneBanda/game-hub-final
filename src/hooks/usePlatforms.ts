148c79682f29b49c5a345e2f27767e7b82504845
ZebroneBanda <zebrone.k@gmail.com>
2024-05-12 19:15:49 +0200

    Resolved the chakra module import error!

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
+import platforms from "../data/platforms";
 
 interface Platform {
   id: number;
@@ -12,6 +13,8 @@ const usePlatforms = () => useQuery({
   queryKey: ['platforms'],
   queryFn: () => 
     apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, //24hrs
+  initialData: {count: platforms.length, results: platforms}
 })
 
 export default usePlatforms;
